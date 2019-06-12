e52f1950d3487ddb70f18b2cde40ef107e78ee9d
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-06-12 14:58:35 -0400

    Changed files w/ little update

--- scripts/badssl-capture.sh
+++ scripts/badssl-capture.sh
@@ -9,13 +9,11 @@ wget --no-check-certificate https://expired.badssl.com
 wget --no-check-certificate https://wrong.host.badssl.com
 wget --no-check-certificate https://self-signed.badssl.com
 wget --no-check-certificate https://untrusted-root.badssl.com
-
 wget --no-check-certificate https://superfish.badssl.com
 wget --no-check-certificate https://edellroot.badssl.com
 wget --no-check-certificate https://dsdtestprovider.badssl.com
 wget --no-check-certificate https://preact-cli.badssl.com
 wget --no-check-certificate https://webpack-dev-server.badssl.com
-
 wget --no-check-certificate https://sha1-intermediate.badssl.com
 wget --no-check-certificate https://rc4.badssl.com
 wget --no-check-certificate https://rc4-md5.badssl.com
@@ -23,16 +21,13 @@ wget --no-check-certificate https://dh480.badssl.com
 wget --no-check-certificate https://dh512.badssl.com
 wget --no-check-certificate https://dh1024.badssl.com
 wget --no-check-certificate https://null.badssl.com
-
 wget --no-check-certificate https://tls-v1-0.badssl.com
 wget --no-check-certificate https://tls-v1-1.badssl.com
 wget --no-check-certificate https://cbc.badssl.com
 wget --no-check-certificate https://3des.badssl.com
-
 wget --no-check-certificate https://revoked.badssl.com
 wget --no-check-certificate https://pinning-test.badssl.com
 wget --no-check-certificate https://invalid-expected-sct.badssl.com
-
 wget --no-check-certificate https://1000-sans.badssl.com
 wget --no-check-certificate https://10000-sans.badssl.com
 wget --no-check-certificate https://sha384.badssl.com
@@ -41,7 +36,6 @@ wget --no-check-certificate https://rsa8192.badssl.com
 wget --no-check-certificate https://no-subject.badssl.com
 wget --no-check-certificate https://no-common-name.badssl.com
 wget --no-check-certificate https://incomplete-chain.badssl.com
-
 wget --no-check-certificate https://tls-v1-2.badssl.com
 wget --no-check-certificate https://sha256.badssl.com
 wget --no-check-certificate https://rsa2048.badssl.com
@@ -49,4 +43,3 @@ wget --no-check-certificate https://ecc256.badssl.com
 wget --no-check-certificate https://ecc384.badssl.com
 wget --no-check-certificate https://extended-validation.badssl.com
 wget --no-check-certificate https://mozilla-modern.badssl.com
-
