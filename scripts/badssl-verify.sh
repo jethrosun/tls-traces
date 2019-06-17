1c01d210085629093286514372a80995b6ec76a5
Jethro Shuwen Sun <jethro.s.sun@gmail.com>
2019-06-17 13:55:44 -0400

    Improved badssl verify so it works as expected

--- scripts/badssl-verify.sh
+++ scripts/badssl-verify.sh
@@ -2,43 +2,44 @@
 
 set -x
 
+
 # bad ssl
-openssl s_client -connect expired.badssl.com:443 -servername badssl.com 
-openssl s_client -connect wrong.host.badssl.com:443 -servername badssl.com 
-openssl s_client -connect self-signed.badssl.com:443 -servername badssl.com 
-openssl s_client -connect untrusted-root.badssl.com:443 -servername badssl.com 
-openssl s_client -connect superfish.badssl.com:443 -servername badssl.com
-openssl s_client -connect edellroot.badssl.com:443 -servername badssl.com
-openssl s_client -connect dsdtestprovider.badssl.com:443 -servername badssl.com
-openssl s_client -connect preact-cli.badssl.com:443 -servername badssl.com
-openssl s_client -connect webpack-dev-server.badssl.com:443 -servername badssl.com
-openssl s_client -connect sha1-intermediate.badssl.com:443 -servername badssl.com
-openssl s_client -connect rc4.badssl.com:443 -servername badssl.com
-openssl s_client -connect rc4-md5.badssl.com:443 -servername badssl.com
-openssl s_client -connect dh480.badssl.com:443 -servername badssl.com
-openssl s_client -connect dh512.badssl.com:443 -servername badssl.com
-openssl s_client -connect dh1024.badssl.com:443 -servername badssl.com
-openssl s_client -connect null.badssl.com:443 -servername badssl.com
-openssl s_client -connect tls-v1-0.badssl.com:443 -servername badssl.com
-openssl s_client -connect tls-v1-1.badssl.com:443 -servername badssl.com
-openssl s_client -connect cbc.badssl.com:443 -servername badssl.com
-openssl s_client -connect 3des.badssl.com:443 -servername badssl.com
-openssl s_client -connect revoked.badssl.com:443 -servername badssl.com
-openssl s_client -connect pinning-test.badssl.com:443 -servername badssl.com
-openssl s_client -connect invalid-expected-sct.badssl.com:443 -servername badssl.com
-openssl s_client -connect 1000-sans.badssl.com:443 -servername badssl.com
-openssl s_client -connect 10000-sans.badssl.com:443 -servername badssl.com
-openssl s_client -connect sha384.badssl.com:443 -servername badssl.com
-openssl s_client -connect sha512.badssl.com:443 -servername badssl.com
-openssl s_client -connect rsa8192.badssl.com:443 -servername badssl.com
-openssl s_client -connect no-subject.badssl.com:443 -servername badssl.com
-openssl s_client -connect no-com:443 -servername badssl.common-name.badssl.com
-openssl s_client -connect incom:443 -servername badssl.complete-chain.badssl.com
-openssl s_client -connect tls-v1-2.badssl.com:443 -servername badssl.com
-openssl s_client -connect sha256.badssl.com:443 -servername badssl.com
-openssl s_client -connect rsa2048.badssl.com:443 -servername badssl.com
-openssl s_client -connect ecc256.badssl.com:443 -servername badssl.com
-openssl s_client -connect ecc384.badssl.com:443 -servername badssl.com
-openssl s_client -connect extended-validation.badssl.com:443 -servername badssl.com
-openssl s_client -connect mozilla-modern.badssl.com:443 -servername badssl.com
+openssl s_client -verify 5 -connect expired.badssl.com:443 -servername expired.badssl.com &
+openssl s_client -verify 5 -connect wrong.host.badssl.com:443 -servername wrong.host.badssl.com &
+openssl s_client -verify 5 -connect self-signed.badssl.com:443 -servername self-signed.badssl.com &
+openssl s_client -verify 5 -connect untrusted-root.badssl.com:443 -servername untrusted-root.badssl.com &
+openssl s_client -verify 5 -connect superfish.badssl.com:443 -servername superfish.badssl.com &
+openssl s_client -verify 5 -connect edellroot.badssl.com:443 -servername edellroot.badssl.com &
+openssl s_client -verify 5 -connect dsdtestprovider.badssl.com:443 -servername dsdtestprovider.badssl.com &
+openssl s_client -verify 5 -connect preact-cli.badssl.com:443 -servername preact-cli.badssl.com &
+openssl s_client -verify 5 -connect webpack-dev-server.badssl.com:443 -servername webpack-dev-server.badssl.com &
+openssl s_client -verify 5 -connect sha1-intermediate.badssl.com:443 -servername sha1-intermediate.badssl.com &
+openssl s_client -verify 5 -connect rc4.badssl.com:443 -servername rc4.badssl.com &
+openssl s_client -verify 5 -connect rc4-md5.badssl.com:443 -servername rc4-md5.badssl.com &
+openssl s_client -verify 5 -connect dh480.badssl.com:443 -servername dh480.badssl.com &
+openssl s_client -verify 5 -connect dh512.badssl.com:443 -servername dh512.badssl.com &
+openssl s_client -verify 5 -connect dh1024.badssl.com:443 -servername dh1024.badssl.com &
+openssl s_client -verify 5 -connect null.badssl.com:443 -servername null.badssl.com &
+openssl s_client -verify 5 -connect tls-v1-0.badssl.com:443 -servername tls-v1-0.badssl.com &
+openssl s_client -verify 5 -connect tls-v1-1.badssl.com:443 -servername tls-v1-1.badssl.com &
+openssl s_client -verify 5 -connect cbc.badssl.com:443 -servername cbc.badssl.com &
+openssl s_client -verify 5 -connect 3des.badssl.com:443 -servername 3des.badssl.com &
+openssl s_client -verify 5 -connect revoked.badssl.com:443 -servername revoked.badssl.com &
+openssl s_client -verify 5 -connect pinning-test.badssl.com:443 -servername pinning-test.badssl.com &
+openssl s_client -verify 5 -connect invalid-expected-sct.badssl.com:443 -servername invalid-expected-sct.badssl.com &
+openssl s_client -verify 5 -connect 1000-sans.badssl.com:443 -servername 1000-sans.badssl.com &
+openssl s_client -verify 5 -connect 10000-sans.badssl.com:443 -servername 10000-sans.badssl.com &
+openssl s_client -verify 5 -connect sha384.badssl.com:443 -servername sha384.badssl.com &
+openssl s_client -verify 5 -connect sha512.badssl.com:443 -servername sha512.badssl.com &
+openssl s_client -verify 5 -connect rsa8192.badssl.com:443 -servername rsa8192.badssl.com &
+openssl s_client -verify 5 -connect no-subject.badssl.com:443 -servername no-subject.badssl.com &
+openssl s_client -verify 5 -connect no-common-name.badssl.com:443 -servername no-common-name.badssl.com &
+openssl s_client -verify 5 -connect incomplete-chain.badssl.com:443 -servername incomplete-chain.badssl.com &
+openssl s_client -verify 5 -connect tls-v1-2.badssl.com:443 -servername tls-v1-2.badssl.com &
+openssl s_client -verify 5 -connect sha256.badssl.com:443 -servername sha256.badssl.com &
+openssl s_client -verify 5 -connect rsa2048.badssl.com:443 -servername rsa2048.badssl.com &
+openssl s_client -verify 5 -connect ecc256.badssl.com:443 -servername ecc256.badssl.com &
+openssl s_client -verify 5 -connect ecc384.badssl.com:443 -servername ecc384.badssl.com &
+openssl s_client -verify 5 -connect extended-validation.badssl.com:443 -servername extended-validation.badssl.com &
+openssl s_client -verify 5 -connect mozilla-modern.badssl.com:443 -servername mozilla-modern.badssl.com &
 
